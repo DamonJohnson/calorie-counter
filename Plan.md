61a05cd0352b64519bd3a4562b0c2291bdde50fe
DamonJohnson <85825667+DamonJohnson@users.noreply.github.com>
2022-06-19 21:37:40 +1000

    added table and add item functionality

--- Plan.md
+++
@@ -1,5 +0,0 @@
-1. Create HTML page
-2. Display Content in Table with columns for Item, Calories and Total Calories
-3. Create a HTML Form and button for adding item
-4. Create a HTML Button for Editing Each Item
-5. Add Event query listeners to the add, edit and delete buttons.
--- script.js
+++ script.js
@@ -1,12 +1,40 @@
+
 // Adding a new item
-const submit = document.querySelector('#submit')
-submit.addEventListener('click', addItem)
+const newButton = document.querySelector('#new-button')
+
+newButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    let item = window.prompt("Enter item name: ")
+    let calories = window.prompt("Enter calories for " + item)
+    addItem(item, calories)
+})
 
 function addItem(item, calories) {
-    event.preventDefault()
     const table = document.querySelector('table')
     let newItem = document.createElement('tr')
     newItem.innerHTML =
-        `<td>${item}</td>`
-        `<td>${calories}</td>`
+        `<td>${item}</td>
+        <td class='calories'>${calories}</td>
+        <td> <button class="edit-button">Edit</button>`
+    table.appendChild(newItem)
+}
+
+// Edit Item
+editButton = document.querySelectorAll('.edit-button').addEventListener('click', editItem)
+
+function editItem() { 
+    console.log('ass')
+}
+
+// Daily Calorie Total
+let total = document.querySelector("#total")
+total.textContent = totalCalories()
+
+function totalCalories() {
+    const table = document.querySelector('table')
+    let totalValue = 0
+    for (i = 1; i < table.rows.length; i++) {
+        console.log(totalValue += (parseInt(table.rows[i].cells[1].innerHTML)))
+    }
+    // return totalValue
 }
